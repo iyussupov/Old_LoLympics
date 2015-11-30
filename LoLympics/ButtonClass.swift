862bd5a7f253d7faaa7d680e2d97e7ab6bccc2e6
Ingwar <iyussupov@gmail.com>
2015-11-30 14:37:46 +0200

    Initial Commit

---
+++ LoLympics/ButtonClass.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonClass.swift
+//  LoLympics
+//
+//  Created by Dev1 on 11/30/15.
+//  Copyright © 2015 FXoffice. All rights reserved.
+//
+
+import UIKit
+
+class ButtonClass: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 9.0
+    }
+
+}
