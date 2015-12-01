5f5689e1f08fdb68cb017dcb8ada97d93175af7d
Ingwar <iyussupov@gmail.com>
2015-12-01 10:23:43 +0200

    Sign Up view finished

---
+++ LoLympics/BtnViewStyle.swift
@@ -0,0 +1,17 @@
+//
+//  BtnViewStyle.swift
+//  LoLympics
+//
+//  Created by Dev1 on 12/1/15.
+//  Copyright © 2015 FXoffice. All rights reserved.
+//
+
+import UIKit
+
+class BtnViewStyle: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 9.0
+    }
+
+}
