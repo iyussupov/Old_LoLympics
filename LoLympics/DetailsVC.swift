a93021fe15c74d669ac70f2e46cac675af76300c
Ingwar <iyussupov@gmail.com>
2015-12-05 17:34:18 +0200

    details updated

--- LoLympics/DetailsVC.swift
+++ LoLympics/DetailsVC.swift
@@ -8,21 +8,81 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+let offset_HeaderStop:CGFloat = 340.0 // At this offset the Header stops its transformations
+
+class DetailsVC: UIViewController, UIScrollViewDelegate, UITableViewDataSource, UITableViewDelegate {
     
     var post: Post!
 
+    @IBOutlet var scrollView:UIScrollView!
+    @IBOutlet var header: UIView!
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var categoryLbl: BadgeViewStyle!
+    @IBOutlet weak var backBtnLbl: RoundBtnViewStyle!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        scrollView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func viewDidAppear(animated: Bool) {
+        
+        headerImage.clipsToBounds = true
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        
+        return tableView.dequeueReusableCellWithIdentifier("CommentCell")!
+            
+        
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        let offset = scrollView.contentOffset.y
+        var headerTransform = CATransform3DIdentity
+        
+        // PULL DOWN -----------------
+        
+        if offset < 0 {
+            
+            let headerScaleFactor:CGFloat = -(offset) / headerImage.bounds.height
+            let headerSizevariation = ((headerImage.bounds.height * (1.0 + headerScaleFactor)) - headerImage.bounds.height)/2.0
+            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+            
+            headerImage.layer.transform = headerTransform
+        }
+            
+            // SCROLL UP/DOWN ------------
+            
+        else {
+            
+            // Header -----------
+            
+            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
+            
+            categoryLbl.layer.transform = headerTransform
+            backBtnLbl.layer.transform = headerTransform
+        }
+        
+        // Apply Transformations
+        
+        headerImage.layer.transform = headerTransform
+    }
 
     @IBAction func DetailsBackBtn(sender: AnyObject) {
         self.navigationController?.popViewControllerAnimated(true)
